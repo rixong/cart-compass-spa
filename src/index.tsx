bd8d307d4e10df8c0e35952788049bac5f806d47
rixong <rixong@gmail.com>
2020-11-17 10:13:10 -0600

    added logger middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, Store } from 'redux';
+import { createStore, applyMiddleware, Store, Middleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
@@ -8,19 +8,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css';
 import './fresca.css';
 
-import { rootReducer, ApplicationState } from './store';
+import { rootReducer, ApplicationState, RootState } from './store';
 import App from './components/App';
 require('bootstrap')
 
 
-// const logger = store => next => action => {
-//   console.log('dispatching', action);
-//   let result = next(action);
-//   console.log('next state', store.getState());
-//   return result;
-// };
+const logger: Middleware<{}, RootState> = store => next => action => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result;
+};
 
-const middlewares = [thunk];
+const middlewares = [thunk, logger];
 
 // if ( process.env.NODE_ENV === 'development') {    
 //   middlewares.push(logger);    
