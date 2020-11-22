a8e0067f04714141dfb667916a057edc01c9cfed
rixong <rixong@gmail.com>
2020-11-22 15:03:56 -0600

    finished item status

--- src/store/lists/reducers.ts
+++ src/store/lists/reducers.ts
@@ -2,16 +2,14 @@
 import {
   ListsActions,
   ListsState,
+  // IList,
+  // IListItem,
   ADDED_NEW_LIST,
-  // SET_CURRENT_LIST,
-  // CHANGED_CURRENT_LIST,
   REMOVED_LIST,
-  // RETRIEVED_LIST_ITEMS,
   ADDED_ITEM_TO_CUR_LIST,
-  IList,
-  IListItem,
-  // REMOVED_ITEMS_FROM_CUR_LIST,
   CHANGED_ITEMS_STATUS,
+  // RETRIEVED_LIST_ITEMS,
+  // REMOVED_ITEMS_FROM_CUR_LIST,
 } from './types';
 
 const intitalState: ListsState = {
@@ -38,19 +36,20 @@ export default function listReducer(
       currentList = { ...state.lists[idx] }
       currentList.listItems = action.payload.items
       return { ...state, lists: [...state.lists.slice(0, idx), currentList, ...state.lists.slice(idx + 1)] };
+
     case CHANGED_ITEMS_STATUS:
-      idx = state.lists.findIndex((list) => list._id === action.payload.curList);
-      currentList = { ...state.lists[idx] }
+      let listIdx = state.lists.findIndex((list) => list._id === action.payload.curList);
+      currentList = { ...state.lists[listIdx] }
       let items = [...currentList.listItems];
       let itemIdx = items.findIndex((ele) => ele._id === action.payload.item._id)
       items = [...items.slice(0, itemIdx), action.payload.item, ...items.slice(itemIdx + 1)]
       currentList.listItems = items;
-      return { ...state, lists: [...state.lists.slice(0, idx), currentList, ...state.lists.slice(idx + 1)] };
-    
-      // case 'REMOVED_ITEMS_FROM_CUR_LIST':
+      return { ...state, lists: [...state.lists.slice(0, idx), currentList, ...state.lists.slice(listIdx + 1)] };
+
+    // case 'REMOVED_ITEMS_FROM_CUR_LIST':
     //   let tempItem = [...state.curListItems].filter(item => item.item_id !== action.payload)
     //   return { ...state, curListItems: tempItem }
-    
+
     default:
       return state
   }
